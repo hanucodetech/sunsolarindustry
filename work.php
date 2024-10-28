2dfa21dc9057b2b92e25c2c9b78d92c37f39eca7
neerajpandit <pandeyneeraj1507@gmail.com>
2024-10-28 13:26:00 +0530

    ini

---
+++ work.php
@@ -0,0 +1,8 @@
+
+<!-- work.php -->
+<main class="container mx-auto py-10 px-4">
+    <section>
+        <h2 class="text-4xl font-semibold mb-4">Our Work</h2>
+        <p>We take pride in our work, delivering solutions that exceed expectations and bring value to our clients.</p>
+    </section>
+</main>
